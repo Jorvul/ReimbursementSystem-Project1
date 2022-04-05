63c083f1294ba8e7f82cfad713b17fef11cacc36
Jorge Fernandez <jorgelfc1989@gmail.com>
2022-04-05 17:32:49 -0400

    created main class

---
+++ src/main/java/com/jorge/classes/Main.java
@@ -0,0 +1,12 @@
+package com.jorge.classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EmployeeClass emp = new EmployeeClass(201,"Phillip Stray","social worker", "Boomer","tooOldForThis100A!");
+		
+		ExpenseClass exp = new ExpenseClass();
+
+	}
+
+}
